0d32f7b781303767b7d94bbb7150e0f495b79935
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-16 10:27:22 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ *
+ * Return: Always (0) - Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return	(0);
+}
