6b12939a9c0bd1e2841b8019ace3513eb72adadc
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-16 10:35:56 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
-di
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ *
+ * Return (0): Always (0) - Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcomes is a piece of art,\n");
+
+	return (0);
+}
