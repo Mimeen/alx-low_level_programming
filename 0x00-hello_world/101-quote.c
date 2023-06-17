fde9ede7df7a805fef4e1c0c6f2a337ce85bcfab
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-17 04:06:41 -0700

    write to err

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, message, 54);
 
 	return (1);
 }
