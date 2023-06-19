430f567cc55cbbda71203b29afa6ddfa652f22de
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-19 10:57:26 -0700

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -16,7 +16,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (alpha = 65; alpha < 71; alpha++)
+	for (alpha = 97; alpha < 122; alpha++)
 		putchar(alpha);
 	putchar('\n');
 
