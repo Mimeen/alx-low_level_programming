1eed8d5e3259c77c68ecd832002bdfa9603e91fd
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-19 10:54:21 -0700

    print in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ *
+ * Return: Always (0) - Success
+ */
+
+int main(void)
+{
+	int num;
+	int alpha;
+
+	num = 0;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (alpha = 65; alpha < 71; alpha++)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
