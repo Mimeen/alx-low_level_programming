957b49d6b11c00078e7f60db9a740df007342780
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-21 03:58:45 -0700

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabets 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (alpha = 97; alpha < 123; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
