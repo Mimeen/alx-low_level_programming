d8ff3d906fe270fa08b428168979481754ea9c64
Mimeen <oyelakinabdullahioyedele@gmail.com>
2023-06-21 03:38:20 -0700

    lower alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Program Entry point
+ *
+ * Return: Always (0) -  Success
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for(alpha = 97; alpha < 123; alpha++)
+		_putchar(alpha);
+
+
+	_putchar('\n');
+}
